5d9edd1a47d7618f8857ae23559aa2269217bcdc
Proser-V <10530@holbertonstudents.com00>
2025-08-12 17:16:34 +0200

    Multi things

--- backend/src/main/java/com/atelierlocal/model/User.java
+++ backend/src/main/java/com/atelierlocal/model/User.java
@@ -1,12 +1,12 @@
 package com.atelierlocal.model;
 
 import jakarta.persistence.*;
-
-import java.util.List;
 import java.util.UUID;
+import java.util.List;
 
 @Entity
-public class Artisan {
+@Table(name = "users")
+public class User {
     // Atributes
 
     @Id
@@ -15,7 +15,10 @@ public class Artisan {
     private UUID id;
 
     @Column(nullable = false)
-    private String name;
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -23,22 +26,28 @@ public class Artisan {
     @Column(name = "hashed_password", nullable = false)
     private String hashedPwd;
 
-    @Column
-    private String category;
-
     @Embedded
     private Address address;
 
     @Column
     private String avatar;
 
-    @OneToMany(mappedBy = "creator")
+    @Column(nullable = false)
+    private Boolean isAdmin;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+
+    @OneToMany(mappedBy = "client")
     private List<UploadedFile> uploadedFiles;
 
     // Getters and setters
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
@@ -46,14 +55,17 @@ public class Artisan {
     public String getHashedPassword() { return hashedPwd; }
     public void setHashedPassword(String hashedPwd) { this.hashedPwd = hashedPwd; }
 
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
-
     public Address getAddress() { return address; }
     public void setAddress(Address address) { this.address = address; }
 
     public String getAvatar() { return avatar; }
     public void setAvatar(String avatar) { this.avatar = avatar; }
+
+    public Boolean getAdmin() { return isAdmin; }
+    public void setAdmin(Boolean isAdmin) { this.isAdmin = isAdmin; }
+
+    public Boolean getActive() { return isActive; }
+    public void setActive(Boolean isActive) { this.isActive = isActive; }
 
     public List<UploadedFile> getUploadedFile() { return uploadedFiles; }
     public void setUploadedFile(List<UploadedFile> uploadedFiles) { this.uploadedFiles = uploadedFiles;}

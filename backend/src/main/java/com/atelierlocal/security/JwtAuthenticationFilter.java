0be0e6827998a7eb9b67d2291224d5e24cfe12fe
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-25 09:52:39 +0200

    Created first security filter, to be improved

---
+++ backend/src/main/java/com/atelierlocal/security/JwtAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.atelierlocal.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, CustomUserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FiterChain fiterChain
+    ) throws ServletException, IOException {
+
+        // Récupération du header Authorization
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+
+        // En cas d'absence de JWT, on passe au filtre suivant
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7); // Retire le "Bearer"
+        username = jwtService.extractUsername(jwt);
+
+        // Vérifier si l'utilisateur est déjà connecté
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            // Vérification du token
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                        );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // On stocke l'auth dans le SecurityContext
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        // On continue la chaîne de filtres
+        filterChain.doFilter(request, response);
+    }
+}

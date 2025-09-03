13d93815c96457c0f80aa7c64065b95129df7d6a
Quentin <48597772+loufi84@users.noreply.github.com>
2025-09-03 11:11:58 +0200

    Corrected the old configuration of User roles

--- backend/src/main/java/com/atelierlocal/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/atelierlocal/security/CustomUserDetailsService.java
@@ -1,12 +1,13 @@
 package com.atelierlocal.security;
 
-import com.atelierlocal.model.Client;
+import com.atelierlocal.model.User;
 import com.atelierlocal.repository.UserRepo;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,20 +21,20 @@ public class CustomUserDetailsService implements UserDetailsService, UserDetails
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Client user = userRepo.findByEmail(email)
+        User user = userRepo.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé : " + email));
 
         return org.springframework.security.core.userdetails.User
                 .withUsername(user.getEmail())
                 .password(user.getHashedPassword()) // ton password hashé
-                .authorities(user.getAdmin() ? "ROLE_ADMIN" : "ROLE_USER")
+                .authorities(new SimpleGrantedAuthority("ROLE_" + user.getUserRole().name()))
                 .accountLocked(!user.getActive()) // si user non actif, compte verrouillé
                 .build();
     }
 
     @Override
     public UserDetails updatePassword(UserDetails user, String newPassword) {
-        Client dbUser = userRepo.findByEmail(user.getUsername())
+        User dbUser = userRepo.findByEmail(user.getUsername())
             .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé : " + user.getUsername()));
 
         dbUser.setHashedPassword(newPassword);
@@ -42,7 +43,7 @@ public class CustomUserDetailsService implements UserDetailsService, UserDetails
         return org.springframework.security.core.userdetails.User
                 .withUsername(dbUser.getEmail())
                 .password(dbUser.getHashedPassword())
-                .authorities(dbUser.getAdmin() ? "ADMIN" : "USER")
+                .authorities(new SimpleGrantedAuthority("ROLE_" + dbUser.getUserRole().name()))
                 .accountLocked(!dbUser.getActive())
                 .build();
     }

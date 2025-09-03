606f699b63cd4535ae447b191183b92bb29b9859
Quentin <48597772+loufi84@users.noreply.github.com>
2025-09-03 17:06:55 +0200

    Ménage des imports dans la config

--- backend/src/main/java/com/atelierlocal/config/S3Config.java
+++ backend/src/main/java/com/atelierlocal/config/S3Config.java
@@ -3,8 +3,6 @@ package com.atelierlocal.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import com.atelierlocal.model.S3Properties;
 

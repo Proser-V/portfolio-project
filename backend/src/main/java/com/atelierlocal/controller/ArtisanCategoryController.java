bb30eb26f6304170e213e0ebca201ba44bfef204
Proser-V <10530@holbertonstudents.com00>
2025-09-29 00:27:36 +0200

    Création des controlleurs de Asking et ArtisanCategory

---
+++ backend/src/main/java/com/atelierlocal/controller/ArtisanCategoryController.java
@@ -0,0 +1,72 @@
+package com.atelierlocal.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atelierlocal.dto.ArtisanCategoryRequestDTO;
+import com.atelierlocal.dto.ArtisanCategoryResponseDTO;
+import com.atelierlocal.dto.ArtisanResponseDTO;
+import com.atelierlocal.service.ArtisanCategoryService;
+import com.atelierlocal.service.ArtisanService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@RestController
+@RequestMapping("/api/artisan-category")
+@Tag(name = "Artisan categories", description = "Définition du controlleur des catégories d'artisans")
+public class ArtisanCategoryController {
+    private ArtisanCategoryService artisanCategoryService;
+    private ArtisanService artisanService;
+
+    public ArtisanCategoryController(ArtisanCategoryService artisanCategoryService, ArtisanService artisanService) {
+        this.artisanCategoryService = artisanCategoryService;
+        this.artisanService = artisanService;
+    }
+
+    @PostMapping("/creation")
+    public ResponseEntity<ArtisanCategoryResponseDTO> createArtisanCategory(ArtisanCategoryRequestDTO request) {
+        ArtisanCategoryResponseDTO newArtisanCategory = artisanCategoryService.createArtisanCategory(request);
+        return ResponseEntity.ok(newArtisanCategory);
+    }
+    
+    @GetMapping("/")
+    public ResponseEntity<List<ArtisanCategoryResponseDTO>> getAllArtisanCategories() {
+        List<ArtisanCategoryResponseDTO> allArtisanCategories = artisanCategoryService.getAllArtisanCategory();
+        return ResponseEntity.ok(allArtisanCategories);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<ArtisanCategoryResponseDTO> getArtisanCategoryById(UUID id) {
+        ArtisanCategoryResponseDTO artisanCategory = artisanCategoryService.getArtisanCategoryById(id);
+        return ResponseEntity.ok(artisanCategory);
+    }
+
+    @GetMapping("/{id}/artisans")
+    public ResponseEntity<List<ArtisanResponseDTO>> getArtisansByCategory(UUID id) {
+        List<ArtisanResponseDTO> artisansByCategory = artisanService.getAllArtisansByCategory(id);
+        return ResponseEntity.ok(artisansByCategory);
+    }
+    
+
+    @PutMapping("/{id}/update")
+    public ResponseEntity<ArtisanCategoryResponseDTO> updateArtisanCategory(UUID id, ArtisanCategoryRequestDTO request) {
+        ArtisanCategoryResponseDTO updatedArtisanCategory = artisanCategoryService.updateArtisanCategory(id, request);
+        return ResponseEntity.ok(updatedArtisanCategory);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<Void> deleteArtisanCategory(UUID id) {
+        artisanCategoryService.deleteArtisanCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ backend/src/main/java/com/atelierlocal/controller/AskingController.java
@@ -0,0 +1,66 @@
+package com.atelierlocal.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atelierlocal.dto.ArtisanResponseDTO;
+import com.atelierlocal.dto.AskingRequestDTO;
+import com.atelierlocal.dto.AskingResponseDTO;
+import com.atelierlocal.service.AskingService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api/askings")
+@Tag(name = "Askings", description = "Définition du controlleur des askings")
+public class AskingController {
+    private final AskingService askingService;
+    
+    public AskingController(AskingService askingService) {
+        this.askingService = askingService;
+    }
+
+    @PostMapping("/creation")
+    public ResponseEntity<AskingResponseDTO> createAsking(AskingRequestDTO request) {
+        AskingResponseDTO newAsking = askingService.createAsking(request);
+        return ResponseEntity.ok(newAsking);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<AskingResponseDTO>> getAllAskings() {
+        List<AskingResponseDTO> allAskings = askingService.getAllAskings();
+        return ResponseEntity.ok(allAskings);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AskingResponseDTO> getAskingById(UUID id) {
+        AskingResponseDTO asking = askingService.getAskingById(id);
+        return ResponseEntity.ok(asking);
+    }
+
+    @PutMapping("/{id}/update")
+    public ResponseEntity<AskingResponseDTO> updateAsking(UUID id, AskingRequestDTO request) {
+        AskingResponseDTO updatedAsking = askingService.updateAsking(id, request);
+        return ResponseEntity.ok(updatedAsking);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<Void> deleteAsking(UUID id) {
+        askingService.deleteAsking(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+

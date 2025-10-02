d53b5897613d1b61391166a94b79b0aa61651f0b
Proser-V <10530@holbertonstudents.com00>
2025-10-02 16:46:29 +0200

    front initialisation

---
+++ frontend/atelier-local/src/App.js
@@ -0,0 +1,13 @@
+import VisitorHeader from "./components/VisitorHeader";
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div>
+      <VisitorHeader/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
